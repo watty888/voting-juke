974dec261dccf0781d81b5cf8d761d827ce5c6e9
Bunyatov Elcin <a01449747@unet.univie.ac.at>
2021-05-06 21:58:00 +0200

    fixed docker mysql error

--- db-spring.sql
+++ db-spring.sql
@@ -1,3 +1,2 @@
-create database [IF NOT EXISTS] db_example;
-create user [IF NOT EXISTS] 'springuser'@'%' identified by 'ThePassword';
+create user 'springuser'@'%' identified by 'ThePassword';
 grant all on db_example.* to 'springuser'@'%';
